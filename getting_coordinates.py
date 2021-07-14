d7495c932b13544147f8ee23d7d39a2603afba66
bronreichardtchu <breichardtchu@swin.edu.au>
2021-07-15 00:05:36 +1000

    Created file for getting the coordinates of the maps

---
+++ getting_coordinates.py
@@ -0,0 +1,24 @@
+"""
+NAME:
+	getting_coordinates.py
+
+AUTHOR:
+	Bronwyn Reichardt Chu
+	Swinburne
+	2021
+
+EMAIL:
+	<breichardtchu@swin.edu.au>
+
+PURPOSE:
+	To read in the data
+	Written on MacOS Mojave 10.14.5, with Python 3.7
+
+FUNCTIONS INCLUDED:
+    read_in_data_fits
+    create_wcs
+
+MODIFICATION HISTORY:
+		v.1.0 - first created August 2019
+
+"""
